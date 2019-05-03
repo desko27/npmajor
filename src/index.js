c540538c5dbfa30ff1d6d73c59d1d5758e615f22
desko27 <desko27@gmail.com>
2019-05-03 11:29:19 +0200

    First working version

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
+const program = require('commander')
 const execa = require('execa')
 const fs = require('fs')
 const semver = require('semver')
 
-const shellArgs = process.argv.slice(2)
-const [name] = shellArgs
-;(async () => {
-  // run the install command
-  // const {stdout} = await execa('npm', ['install', name])
-  await execa('npm', ['install', name])
+const {version} = require('../package.json')
 
-  // read package.json
-  const rawPackageJson = fs.readFileSync('package.json')
-  const packageJson = JSON.parse(rawPackageJson)
-  console.log('installed', packageJson.dependencies[name])
+const DEPENDENCIES_KEYS = {DEV: 'devDependencies', NORMAL: 'dependencies'}
 
-  // update package.json
-  const version = packageJson.dependencies[name]
-  packageJson.dependencies[name] = `${semver.coerce(version).major}`
-  const updatedPackageJson = JSON.stringify(packageJson, null, 2)
-  fs.writeFileSync('package.json', updatedPackageJson)
+program
+  .version(version)
+  .command('install <packages...>')
+  .alias('i')
+  .option('-D, --save-dev', 'Save to devDependencies')
+  .action(async function(packages, {saveDev}) {
+    // run the install command
+    await execa('npm', ['install', saveDev && '--save-dev', ...packages]) // const {stdout} = await execa('npm', ['install', name])
 
-  console.log('updated', packageJson.dependencies[name])
-})()
+    // read package.json
+    const rawPackageJson = fs.readFileSync('package.json')
+    const packageJson = JSON.parse(rawPackageJson)
+
+    // replace versions of installed packages on package.json object
+    Object.values(DEPENDENCIES_KEYS).forEach(dependenciesKey => {
+      packages.forEach(pkg => {
+        const dependencies = packageJson[dependenciesKey]
+        if (!dependencies) return
+
+        const version = dependencies[pkg]
+        if (!version) return
+
+        dependencies[pkg] = `${semver.coerce(version).major}`
+      })
+    })
+
+    // commit changes to package.json file
+    const updatedPackageJson = JSON.stringify(packageJson, null, 2)
+    fs.writeFileSync('package.json', updatedPackageJson)
+  })
+
+program.parse(process.argv)
