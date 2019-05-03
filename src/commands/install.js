a9f16eabeaa1eba3c5da34c3c38f8657b4cb852d
desko27 <desko27@gmail.com>
2019-05-03 12:40:19 +0200

    Add final output results via listr context

--- src/commands/install.js
+++ src/commands/install.js
@@ -15,12 +15,13 @@ module.exports = async function install(packages, {saveDev}) {
     },
     {
       title: 'Update versions on package.json',
-      task: () => {
+      task: ctx => {
         // read package.json
         const rawPackageJson = fs.readFileSync('package.json')
         const packageJson = JSON.parse(rawPackageJson)
 
         // replace versions of installed packages on package.json object
+        const history = {}
         Object.values(DEPENDENCIES_KEYS).forEach(dependenciesKey => {
           packages.forEach(pkg => {
             const dependencies = packageJson[dependenciesKey]
@@ -29,18 +30,36 @@ module.exports = async function install(packages, {saveDev}) {
             const version = dependencies[pkg]
             if (!version) return
 
-            dependencies[pkg] = `${semver.coerce(version).major}`
+            // replace full version by major one
+            const majorVersion = `${semver.coerce(version).major}`
+            dependencies[pkg] = majorVersion
+            history[pkg] = {version, majorVersion}
           })
         })
 
         // commit changes to package.json file
         const updatedPackageJson = JSON.stringify(packageJson, null, 2)
         fs.writeFileSync('package.json', updatedPackageJson)
+
+        // send results to the output
+        ctx.history = history
       }
     }
   ])
 
-  tasks.run().catch(err => {
-    console.error(err)
-  })
+  tasks
+    .run()
+    .catch(err => {
+      console.error(err)
+    })
+    .then(ctx => {
+      const {history} = ctx
+
+      // output results
+      console.log()
+      Object.entries(history).forEach(([pkg, {version, majorVersion}]) => {
+        console.log(`  • ${pkg}: ${version} → ${majorVersion}`)
+      })
+      console.log()
+    })
 }
